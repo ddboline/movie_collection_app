70cd402c396d997cf1d422bddb64ecf57500a09c
Daniel Boline <ddboline@gmail.com>
2016-04-21 20:56:05 -0400

    ...

--- sync_sabrent_with_nas.py
+++ sync_sabrent_with_nas.py
@@ -3,7 +3,8 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 import os
 
-from movie_collection_app.sync_sabrent_with_nas import sync_sabrent_with_nas
+#from movie_collection_app.sync_sabrent_with_nas import sync_sabrent_with_nas
+from movie_collection_app.sync_sabrent_with_nas import sync_queue_with_nas
 
 
 if __name__ == '__main__':
@@ -11,4 +12,4 @@ if __name__ == '__main__':
     for arg in os.sys.argv:
         if arg == 'dry_run':
             dry_run = True
-    sync_sabrent_with_nas(run_command=(not dry_run))
+    sync_queue_with_nas(run_command=(not dry_run))
