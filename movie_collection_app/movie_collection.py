859fc772491aeb037cec6631a35bf20d0c19f9a0
Daniel Boline <ddboline@gmail.com>
2016-04-21 20:56:30 -0400

    update airdate

--- movie_collection_app/movie_collection.py
+++ movie_collection_app/movie_collection.py
@@ -147,16 +147,17 @@ class MovieCollection(object):
                 episode_ = itdict['episode']
                 epurl_ = itdict['epurl']
                 rating_ = itdict['rating']
+                airdate_ = itdict['airdate']
                 itdict['eptitle'] = re.sub('[^A-Za-z0-9 ]', '',
                                            itdict['eptitle'])
                 tmp = self.imdb_episode_ratings.get(show, {})
                 if tmp and tmp.get((season_, episode_), {}):
                     self.con.execute("UPDATE imdb_episodes SET "
-                                     "rating=%d, epurl='%s' "
+                                     "rating=%d, epurl='%s', airdate='%s' "
                                      "WHERE show = '%s' and season = %d "
                                      "and episode = %d" % (
-                                         rating_, epurl_, show, season_,
-                                         episode_))
+                                         rating_, epurl_, airdate_, show,
+                                         season_, episode_))
                 else:
                     keys, values = zip(*itdict.items())
                     keys = ', '.join('%s' % x for x in keys)
