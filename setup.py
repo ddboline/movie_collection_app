c7a9a33b406e49761bbd8622072b35556e2c0070
Daniel Boline <ddboline@gmail.com>
2017-06-06 19:05:32 -0400

    version bump

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ else:
 
 setup(
     name='movie_collection_app',
-    version='0.0.1.9',
+    version='0.0.2.0',
     author='Daniel Boline',
     author_email='ddboline@gmail.com',
     description='movie_collection_app',
