84229de1fc38fbbdaa880476ba6876249391b977
Daniel Boline <ddboline@gmail.com>
2017-11-04 16:11:48 -0400

    ...

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ else:
 
 setup(
     name='movie_collection_app',
-    version='0.0.3.5',
+    version='0.0.3.6',
     author='Daniel Boline',
     author_email='ddboline@gmail.com',
     description='movie_collection_app',
